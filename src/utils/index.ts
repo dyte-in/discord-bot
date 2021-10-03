e753d0ea27dae196afc9be4109fb5d93f6ab1b4e
roerohan <roerohan@gmail.com>
2021-10-03 15:05:47 +0530

    fix(utils): export utils from single index.ts file

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { default as config } from './config';
+export { default as logger } from './logger';
+export { default as getMeetingLink } from './meeting';
+export { default as Requests } from './request';
