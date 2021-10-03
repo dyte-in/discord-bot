8e59c51b1efeceece5d37dfa9393d9f8ec7b49ce
roerohan <roerohan@gmail.com>
2021-10-04 03:06:29 +0530

    feat(announce): added basic $announce command functionality, to be changed to accept args

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,4 +1,5 @@
 import { Collection } from 'discord.js';
+import announce from './announce';
 import Command from './command';
 import dyte from './dyte';
 
@@ -8,5 +9,6 @@ const commands = new Collection<string, Command>();
 
 commands.set(Ping.command.name, Ping);
 commands.set(dyte.command.name, dyte);
+commands.set(announce.command.name, announce);
 
 export default commands;
